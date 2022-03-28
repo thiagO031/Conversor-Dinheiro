5400993b0f0ab3337ae647940ab9b666e832046e
thiagO031 <thiago.tsantana3@gmail.com>
2022-03-28 19:44:48 -0300

    finalizações

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 function converter(){
+    // ID´S LINKADOS PARA O STYLE
    let corp = document.getElementById('corpo')
    let m = document.getElementById('principal')
 
+    // ID´S LINKADOS PARA CONVERSÃO
     let b_number = Number(document.getElementById('btn-number').value);
     let r = document.getElementById('res')
 
+    // CRIANDO VARIAVEL CONV COM O B_NUMBER E O VALOR ATUAL DO DOLAR
     let conv = Number(b_number * 4.76)
     
-    let desc = "O valor convertido é: " + "US$" + conv
-    // let desc = "R$ " + b_number + "  ->>  US$ " + conv 
-    
-    r.innerHTML = desc
+    // SE O VALOR FOR 0 OU MENOR QUE O
+    if(b_number == 0 || b_number < 0){
+        alert('Digite um valor para a conversão!')
+    }
 
+    //CONTEUDO NO r (res) 
+    r.innerHTML = "O valor convertido é: " + "US$" + conv
+    // let desc = "O valor convertido é: " + "US$" + conv
+    
+
+    // STYLE 
     // r.style.backgroundColor = 'white'
     r.style.backgroundColor = '#005db9'
+    r.style.boxShadow = '0 0 0.5m #00224e'
     corp.style.backgroundColor = '#c42021'
     m.style.backgroundColor = '#296eb4'
 
