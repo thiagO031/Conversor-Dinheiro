ba55fc816c3adde63ed3d1c48a3eeff089a49956
thiagO031 <thiago.tsantana3@gmail.com>
2022-03-28 17:43:47 -0300

    javascript atualizado

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function converter(){
+   let corp = document.getElementById('corpo')
+   let m = document.getElementById('principal')
+
     let b_number = Number(document.getElementById('btn-number').value);
     let r = document.getElementById('res')
 
-    let conv = Number(b_number * 5)
+    let conv = Number(b_number * 4.76)
     
     let desc = "O valor convertido é: " + "US$" + conv
     // let desc = "R$ " + b_number + "  ->>  US$ " + conv 
     
     r.innerHTML = desc
 
+    // r.style.backgroundColor = 'white'
+    r.style.backgroundColor = '#005db9'
+    corp.style.backgroundColor = '#c42021'
+    m.style.backgroundColor = '#296eb4'
+
 }
+
